c821985942e23ae2b99733ee477bbf6bb1ed2d10
Jeff Su <me@jeffsu.com>
2014-03-20 14:05:22 -0700

    adding proper stop time on proc page

---
+++ examples/child.js
@@ -0,0 +1,1 @@
+setInterval(function () { console.log("child") }, 1000)
--- examples/hello.js
+++ examples/hello.js
@@ -1,1 +1,3 @@
-setInterval(function () { console.log("hello") }, 1000)
+setInterval(function () { console.log("hello") }, 1000);
+var cp = require('child_process');
+var child = cp.spawn("node", [ "./examples/child.js" ], { stdio: 'inherit' });
