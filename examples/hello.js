7b8dfe891f56c60e5a11a74b2f1a8ca6deaab38c
Sheldon Kwok <me@sheldonk.com>
2014-05-21 15:47:42 -0700

    update example

--- examples/hello.js
+++ examples/hello.js
@@ -1,3 +1,4 @@
+console.log('starting');
 setInterval(function () { console.log("hello") }, 1000);
 var cp = require('child_process');
 var child = cp.spawn("node", [ "./examples/child.js" ], { stdio: 'inherit' });
